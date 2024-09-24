7796d8b3e2764dd56b5a4fedf3993637715176aa
Maxi Diaz <maxi.diaz66@gmail.com>
2024-09-23 21:25:39 -0300

    chore: add OrthographyPage structure

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,5 @@
+export { TextMessageBox } from "./chat-input-boxes/TextMessageBox";
+export { TypingLoader } from "./loaders/TypingLoader";
+export { MyMessage } from "./my-message/MyMessage";
+export { GptMessage } from "./chat-bubbles/GptMessage";
+export { SideBarMenuItem } from "./sidebar/SideBarMenuItem";
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,6 +1,27 @@
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
 
 export const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
-  )
-}
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-y2">
+          {/* Bienvenida */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones"></GptMessage>
+          <MyMessage text="Hola mundo" />
+          <TypingLoader className="fade-in" />
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={(message) => console.log(message)}
+        placeholder="Escribe aquí lo que deseas"
+        disableCorrections
+      />
+    </div>
+  );
+};
